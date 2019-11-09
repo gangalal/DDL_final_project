bb3800c9472c29c1ff801d370b3175741f2d92bb
gangalal <38142529+gangalal@users.noreply.github.com>
2019-11-08 20:42:58 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -21,6 +21,8 @@
 
 #define FIO2DIR (*(volatile unsigned int *)0x2009c040)
 #define FIO2PIN (*(volatile unsigned int *)0x2009c054)
+#define FIO2PIN0 (*(volatile unsigned int *)0x2009C054)
+#define DACR (*(volatile unsigned int *)(0x4008C000)) 		// DAC Register
 
 void GPIOInit() {
 	FIO2DIR |= (1 << 0); // configure P2[0] as output
@@ -46,41 +48,59 @@ void GPIOInit() {
 	FIO2PIN &= ~(1 << 11);
 }
 
-
-void sendInit() {
-	FIO2PIN |= (0 << 0);
-	FIO2PIN |= (0 << 1);
-	FIO2PIN |= (1 << 2);
-	FIO2PIN |= (1 << 3);
-	FIO2PIN |= (1 << 4);
-	FIO2PIN |= (0 << 5);
-	FIO2PIN |= (0 << 6);
-	FIO2PIN |= (0 << 7);
-	FIO2PIN |= (1 << 11); //Rs 1
+// Configure LCD Instruction for commands
+void LCDCmd(int data) {
+	FIO2PIN &= ~(1 << 11); // Set Rs = 0 for cmd (clear bit 11)
+	FIO2PIN0 = data; // set up DB0 - DB7
+	FIO2PIN |= (1 << 8); // pulse E high
+	FIO2PIN &= ~(1 << 8); // pulse E low
+	wait_us(100); // wait 100 usec
 }
 
-void sendComand() {
-	FIO2PIN |= (0 << 0);
-	FIO2PIN |= (0 << 1);
-	FIO2PIN |= (0 << 2);
-	FIO2PIN |= (0 << 3);
-	FIO2PIN |= (0 << 4);
-	FIO2PIN |= (0 << 5);
-	FIO2PIN |= (0 << 6);
-	FIO2PIN |= (1 << 7);
-	FIO2PIN &= ~(1 << 11); //Rs low
+// Initialize LCD (COMMANDS) ** optional can call from main as well
+void LCDinitCmd(void) {
+	LCDCmd(0x38); // Function Set
+	LCDCmd(0x0E); // Turn display on, cursor on, no cursor blinking
+	LCDCmd(0x06); // Entry mode cmd, cursor move L to R
+	LCDCmd(0x01); // clear display
+	wait_us(4000); // wait 4ms to clear display
+}
+
+// Configure LCD Instruction for Characters
+void LCDchar(int data) {
+	FIO2PIN |= (1 << 11); // Set Rs = 1 for character (bit 11 goes high)
+	FIO2PIN0 = data; // set up DB0 - DB7 for ASCII codes for characters
+	FIO2PIN |= (1 << 8); // pulse E high
+	FIO2PIN &= ~(1 << 8); // pulse E low
+	wait_us(100); // wait 100 usec
+}
+
+// Initialize LCD (CHARACTERS) ** optional can call from main as well
+void LCDinitChar(void) {
+	LCDCmd(0x38); // Function Set
+	LCDCmd(0x0E); // Turn display on, cursor on, no cursor blinking
+	LCDCmd(0x06); // Entry mode cmd, cursor move L to R
+	LCDCmd(0x01); // clear display
+	wait_us(4000); // wait 4ms to clear display
+}
+
+//*** NEED TO CREATE A 3RD FUNCTION SIMILAR TO THOSE ABOVE TO TAKE IN A STRING AND DISPLAY ALL CHARACTERS***
+
+void displayChar(int data) {
+	FIO2PIN0 = data;
+	FIO2PIN |= (1 << 11); //set Rs high
+	FIO2PIN |= (1 << 8); // pulse E high
+	FIO2PIN &= ~(1 << 8); // pulse E low
+	wait_us(100); // wait 100 usec
 }
 
 int main(void) {
 	GPIOInit();
-	sendInit();
-	wait_us(100);
-	sendComand();
-	FIO2PIN |= (1 << 8); //E high
-	FIO2PIN &= ~(1 << 8); // E low
-	wait_us(100);
+	LCDinitCmd();
+	LCDinitChar();
+	displayChar(0x41);
 	while (1) {
-    printf("hello world\n");
+		printf("hello world\n");
 	}
 	return 0;
 }
