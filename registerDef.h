2cf7accce479ac2d72a02a7183a561873f0a32bc
jesshorner <55520859+jesshorner@users.noreply.github.com>
2019-11-26 11:24:10 -0600

    Update registerDef.h
    Added I2C register definitions

--- registerDef.h
+++ registerDef.h
@@ -16,13 +16,19 @@
 #define PINSEL1 (*(volatile unsigned int *)(0x4002C004))
 #define PINMODE0 (*(volatile unsigned int *)(0x4002C040))
 
-#define T0TCR (*(volatile unsigned int *)(0x40004004)) 		// Timer 0
-#define T0TC (*(volatile unsigned int *)(0x40004008))		// Timer Counter
-
 #define FIO2DIR (*(volatile unsigned int *)0x2009c040)		// GPIO 2
 #define FIO2PIN (*(volatile unsigned int *)0x2009c054)
 #define FIO2PIN0 (*(volatile unsigned char *)0x2009C054)
 
+#define I2CPADCFG (*(volatile unsigned int *)(0x4002C07C))  // I2C for EEProm
+#define I2C0SCLH (*(volatile unsigned int *)(0x4001C010))
+#define I2C0SCLL (*(volatile unsigned int *)(0x4001C014))
+#define I2C0CONSET (*(volatile unsigned int *)(0x4001C000))
+#define I2C0CONCLR (*(volatile unsigned int *)(0x4001C018))
+#define I2C0DAT (*(volatile unsigned int *)(0x4001C008))
+
+#define T0TCR (*(volatile unsigned int *)(0x40004004))      // Timer 0
+#define T0TC (*(volatile unsigned int *)(0x40004008))		// Timer Counter
 #define T2TCR (*(volatile unsigned int *)(0x40090004)) 		// Timer 2 Timer Control Register
 #define T2TC (*(volatile unsigned int *)(0x40090008)) 		// Timer 2 Timer Counter
 #define T2EMR (*(volatile unsigned int *)(0x4009003C)) 		// EMR Match Register for Timer 2
@@ -35,7 +41,7 @@
 #define U0LCR (*(volatile unsigned int *)(0x4000C00C))
 #define U0DLL (*(volatile unsigned int *)(0x4000C000))
 #define U0DLM (*(volatile unsigned int *)(0x4000C004))
-#define U0FDR (*(volatile unsigned int *)(0x4000C028)) //fractional dividing register
+#define U0FDR (*(volatile unsigned int *)(0x4000C028))      //fractional dividing register
 #define U0FCR (*(volatile unsigned int *)(0x4000C008))
 #define U0ACR (*(volatile unsigned int *)(0x4000C020))
 #define U0TER (*(volatile unsigned int *)(0x4000C030))
