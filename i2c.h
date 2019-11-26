842a6717c057b3af6f3038d005ea627fb4173fce
jesshorner <55520859+jesshorner@users.noreply.github.com>
2019-11-26 11:11:51 -0600

    Create i2c.h

---
+++ i2c.h
@@ -0,0 +1,91 @@
+/*
+===============================================================================
+ Name        : keypadTest.c
+ Author      : Daniel Glover
+ Version     : Alpha
+ Copyright   : $(copyright)
+ Description : ** Needs description still **
+===============================================================================
+*/
+
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#include <cr_section_macros.h>
+
+#include <stdio.h>
+
+#include <timer.h>
+#include "registerDef.h"
+
+/*
+ * I2C initialize
+ */
+void I2CInit() {
+	PCONP |= (1 << 7); //resetting power control for I2C0
+	PCLKSEL0 &= ~(1 << 15) | (1 << 14); // resetting clock selection
+	PCLKSEL0 |= (0 << 15) | (0 << 14); //dividing CCLK by 8 *needs to review
+	// set high and low clk time for 1MHz clk/10 = 100kHz
+	I2C0SCLH = 5;
+	I2C0SCLL = 5;
+	PINSEL1 |= (1 << 22) | (0 << 23); //enabling SDA0 on pin p0.27
+	PINSEL1 |= (1 << 24) | (0 << 25); //enabling SCL0 on pin p0.28
+	//I2CPADCFG |= (0<<2); //configuring I2C0 register to standard mode
+	I2C0CONSET = (1 << 6);
+}
+
+/*
+ * I2C Start Function
+ */
+void I2CStart() {
+	I2C0CONSET = (1 << 3); //set SI while configuring other bits
+	I2C0CONSET = (1 << 5); //set STA to start condition
+	I2C0CONCLR = (1 << 3); //clear SI to active state machine
+	while (((I2C0CONSET >> 3) & 1) == 0) {
+		//wait for start condition to be set
+	}
+	I2C0CONCLR = (1 << 5); //clear STA
+}
+
+/*
+ * I2C write function
+ */
+void I2CWrite(int data) {
+	I2C0DAT = data;
+	I2C0CONCLR = (1 << 3); //clear SI to standard data
+	while (((I2C0CONSET >> 3) & 1) == 0) {
+		//wait for write condition to be set
+	}
+
+	//check I2C0STAT for ack or nak
+}
+
+/*
+ * I2C read function
+ */
+int I2CRead(int ack) {
+	if (ack) {
+		I2C0CONSET = (1 << 2);
+	} else {
+		I2C0CONCLR = (1 << 2);
+	}
+	//I2C0CONCLR = (1 << 2);
+	I2C0CONCLR = (1 << 3); //clear SI to start read
+	while (((I2C0CONSET >> 3) & 1) == 0) {
+		//wait for read condition to be set
+	}
+
+	return I2C0DAT;
+}
+
+/*
+ * I2C Stop Function
+ */
+void I2CStop() {
+	I2C0CONSET = (1 << 4); //set STO to request stop condition
+	I2C0CONCLR = (1 << 3); //clear SI to active state machine
+	while (((I2C0CONSET >> 4) & 1) == 1) {
+		//wait for stop condition to be set
+	}
+}
