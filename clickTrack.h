b0d6c64bc382ebb7962d9ec23eca3fa6dad4b17b
jesshorner <55520859+jesshorner@users.noreply.github.com>
2019-11-26 13:10:54 -0600

    Update clickTrack.h

--- clickTrack.h
+++ clickTrack.h
@@ -8,6 +8,25 @@
 #ifndef CLICKTRACK_H_
 #define CLICKTRACK_H_
 
+/*
+ * Generate a slow click track
+ */
+void slowClick(void) {
 
+}
+
+/*
+ * Generate a normal click track
+ */
+void normalClick(void) {
+
+}
+
+/*
+ * Generate a fast click track
+ */
+void fastClick(void) {
+
+}
 
 #endif /* CLICKTRACK_H_ */
